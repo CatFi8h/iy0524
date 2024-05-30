cd507ae007affc7195ac03c6723031c14a9336da
Igor Yurchenko <igor.yurchenko@onsolve.com>
2024-05-29 22:59:07 -0700

    init commit

---
+++ src/main/java/dev/yurchenko/iy0524/Iy0524Application.java
@@ -0,0 +1,13 @@
+package dev.yurchenko.iy0524;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iy0524Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Iy0524Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/yurchenko/iy0524/Iy0524ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yurchenko.iy0524;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iy0524ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
